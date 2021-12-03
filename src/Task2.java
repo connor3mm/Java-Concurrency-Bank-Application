c92fe0f137da84bf78df2f79e9a590d7c30cce63
tamara <tamara.jankova.2020@strath.uni.ac.uk>
2021-12-03 15:50:40 +0000

    Added filter method to filter by thread group

--- src/Task2.java
+++ src/Task2.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Task2 {
 
@@ -13,13 +10,13 @@ public class Task2 {
         }
 
 
-        Thread[] threads = new Thread[rootGroup.activeCount()+1];
-        while (rootGroup.enumerate(threads, true ) == threads.length) {
+        Thread[] threads = new Thread[rootGroup.activeCount() + 1];
+        while (rootGroup.enumerate(threads, true) == threads.length) {
             threads = new Thread[threads.length * 2];
         }
 
-        int i=0;
-        while (threads[i]!=null){
+        int i = 0;
+        while (threads[i] != null) {
             System.out.println("Name of thread: " + threads[i].getName());
             System.out.println("ID of thread: " + threads[i].getId());
             System.out.println("Priority of thread: " + threads[i].getPriority());
@@ -33,30 +30,57 @@ public class Task2 {
     }
 
 
-    public static Thread searchByName(String name){
-        for(Thread t : Thread.getAllStackTraces().keySet()) {
-            if (t.getName().equals(name)){
+    public static Thread searchByName(String name) {
+        for (Thread t : Thread.getAllStackTraces().keySet()) {
+            if (t.getName().equals(name)) {
                 return t;
             }
         }
         return null;
     }
 
+    public static HashMap filterByThreadGroup(String name) {
+
+        HashMap<Integer, Thread> newHashMap = new HashMap<>();
+        int i = 0;
+        for (Thread t : Thread.getAllStackTraces().keySet()) {
+            if (t.getThreadGroup().getName().equals(name)) {
+                newHashMap.put(i, t);
+                i++;
+            }
+        }
+        return newHashMap;
+    }
+
     public static void main(String[] args) {
+        System.out.println("Task 2 Part 2");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the name of the thread: ");
         String name = scanner.nextLine();
         Thread nameThread = searchByName(name);
         System.out.println(nameThread);
 
-        while(true){
+        System.out.println("--------------------" + "\n");
+
+        System.out.println("Enter the name of the thread group: ");
+        String name2 = scanner.nextLine();
+
+        HashMap<Integer, Thread> newHashMap2;
+        newHashMap2 = filterByThreadGroup(name2);
+
+        newHashMap2.forEach((key, value) -> System.out.println(key + " " + value));
+
+        System.out.println("--------------------" + "\n");
+
+        System.out.println("Task 2 Part 1");
+        while (true) {
             getThreads();
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
-            System.out.println("-------------------------------------------");
+            System.out.println("--------------------");
         }
     }
 }
